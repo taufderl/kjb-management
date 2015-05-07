6d4041375acb52c1ce8b06d76f5ffebfd222a359
tadl <tadl@taufderl.de>
2015-05-07 23:06:06 +0200

    Added scouts, children and tents with references

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,17 @@ User.delete_all
 User.create([{name: 'Lena'}, {name: 'Johannes'}])
 
 Tent.delete_all
-
 (1..12).each do |no|
   Tent.create(number: no)
 end
+
+Child.delete_all
+(1..50).each do |no|
+  Child.create({first_name: "child_#{no}", last_name: "child_#{no}", birthday: Date.today, tent: Tent.all[rand(0..11)]})  
+end
+
+Scout.delete_all
+(1..15).each do |no|
+  Scout.create({first_name: "GL_#{no}", last_name: "GL_#{no}", birthday: Date.today - 21.years})  
+end
+
