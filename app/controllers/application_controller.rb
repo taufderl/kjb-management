98ece6cf48e6f56991e33c2f919f4d21774b76d0
tadl <tadl@taufderl.de>
2015-07-11 13:07:14 +0200

    Added warn message if no user selected

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   before_action :set_date_if_not_set
+  before_action :ask_for_user_if_not_set
   
   def set_date_if_not_set
     if not session[:date]
       session[:date] = Date.today.strftime("%d.%m.%Y")
     end
+  end
+  
+  def ask_for_user_if_not_set
+    flash[:alert] = "Please tell me who you are before you continue! Select a user in the upper right corner."
   end
 
 end
