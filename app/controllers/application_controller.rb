f0b447347bb0341c6d1b0969ce0dfb9c282955ea
tadl <tadl@taufderl.de>
2015-07-11 12:22:04 +0200

    Current date as default if none set yet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_action :set_date_if_not_set
+  
+  def set_date_if_not_set
+    if not session[:date]
+      session[:date] = Date.today.strftime("%d.%m.%Y")
+    end
+  end
 
 end
