a46e8f133a077975cbef8736ef958934a658ed4f
unknown <lena-bitbucket@auf-der-landwehr.com>
2015-07-22 19:03:07 +0200

    Versuche fuer Geburtstag

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,9 +4,28 @@ class DashboardController < ApplicationController
     @disbursement = Disbursement.new
     @news = News.new
     @latest_news = News.last(5).reverse
+    
+    @date = Date.strptime(session[:date], "%d.%m.%Y").to_formatted_s(:short)
+    
+    # Versuch 1
+    # @birthday_child = Array.new
+    # @birthday_child.insert(Child.where{Date.strptime(birthday.strftime("%F"),"%Y-%m-%d").to_formatted_s(:short).eql? @date})
+    
+    # Versuch 2
+    @recent_month = Date.strptime(session[:date], "%d.%m.%Y").mon
+    @recent_day = Date.strptime(session[:date], "%d.%m.%Y").mday
+    @children = Child.all
+    @birthday_child = Array.new
+    
+    for element in @children do
+      if (element.birthday.mon == @recent_month && element.birthday.mday == @recent_day)
+        @birthday_child.insert(element)
+      end
+    end
+   
   end
   
-  def select_user
+  def select_users
     if params[:user]
       session[:user] = params[:user]
     end
