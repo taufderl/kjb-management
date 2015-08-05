b0020e43037c6d90bb61b717b387f67f565215a9
tadl <tadl@taufderl.de>
2015-08-05 21:00:54 +0200

    update updated_by field when booking is edited

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -45,7 +45,7 @@ class BookingsController < ApplicationController
   
   def update
     respond_to do |format|
-      if @booking.update(booking_params)
+      if @booking.update(booking_params.merge({updated_by: @user}))
         format.html { redirect_to @booking, notice: 'Booking was successfully updated.' }
         format.json { render :show, status: :ok, location: @booking }
       else
