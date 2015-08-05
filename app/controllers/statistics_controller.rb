ca5060cd08c024430b8ecd06c67fb51c3ceddc75
tadl <tadl@taufderl.de>
2015-08-05 22:40:20 +0200

    Second data field in highcharts

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,7 +1,8 @@
 class StatisticsController < ApplicationController
   def index
     @dates = (Date.parse(Setting.find_by(key: :start_date).value)..Date.parse(Setting.find_by(key: :end_date).value)).to_a
-    @beer_consumption = [1,2,3,4,5]
 
+    @beer_consumption = [1,2,3,4,5]
+    @sausage_consumption = [1,6,3,2,1]
   end
 end
